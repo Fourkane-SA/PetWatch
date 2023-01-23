e05cb3185dc4b4c357bc93d520adbc5242d5cef6
Océane Kalambay <oceane.kalambay@etu.univ-lyon1.fr>
2023-01-23 14:43:30 +0100

    Avancement navigation etapes ajout animal

--- app/screens/addAnimal.tsx
+++ app/screens/addAnimal.tsx
@@ -1,99 +1,169 @@
 import React, { Component, useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, TextInput, View, Image, Button, TouchableOpacity, ListViewBase, FlatList} from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TextInput, View, Image, Button, TouchableOpacity, ListViewBase, FlatList } from 'react-native';
 
-export default  class AddAnimal extends Component {
-    poids = [
-        {
-          gabarit: 'Petit',
-          tranche: '0-7 kg',
-        },
-        {
-            gabarit: 'Moyen',
-            tranche: '7 - 18 kg',
-        },
-        {
-            gabarit: 'Grand',
-            tranche: '18 - 45 kg',
-        },
-        {
-            gabarit: 'Géant',
-            tranche: '45+ kg',
-        },
-      ];
+export default class AddAnimal extends Component {
+  poids = [
+    {
+      gabarit: 'Petit',
+      tranche: '0-7 kg',
+    },
+    {
+      gabarit: 'Moyen',
+      tranche: '7 - 18 kg',
+    },
+    {
+      gabarit: 'Grand',
+      tranche: '18 - 45 kg',
+    },
+    {
+      gabarit: 'Géant',
+      tranche: '45+ kg',
+    },
+  ];
 
-    etape = 1;
-    titleText="Ajoutez votre animal " + this.etape + "/4";
-
-    incrementeEtape() {
-        return this.etape += 1;
-    }
+  state = {
+    etape: 1
+  }
 
   render() {
-    return (    
-    <SafeAreaView style={styles.container}>
-        <View style={styles.etape1}>
+    return (
+      <SafeAreaView style={styles.container}>
+        {this.state.etape< 5 &&
+          <Text style={styles.pageTitle}> Ajoutez votre animal {this.state.etape}/4</Text>
+        }
+
+        {this.state.etape == 1 &&
+          <View style={styles.etape}>
             <View style={styles.bloc}>
-                <Text style={styles.pageTitle}> {this.titleText}</Text> 
-                <TextInput placeholder='Nom animal' style={styles.btnInput}></TextInput>
+              <TextInput placeholder='Nom animal' style={styles.btnInput}></TextInput>
             </View>
 
-            {/* Faire les boutons radio dans un nouveau composant  */}
+            {/* Faire les boutons radio dans un nouveau composant */}
             <View style={styles.blocRadio}>
-                <View style={styles.blocRadioGroup}>
-                    <Text style={styles.txtRadio}> Mâle</Text> 
-                    <Text style={styles.txtRadio}> Femelle</Text> 
-                </View>
-                <View style={styles.blocRadioGroup}>
-                    <Text style={styles.txtRadio}> Chien</Text> 
-                    <Text style={styles.txtRadio}> Chat</Text>
-                </View>
+              <View style={styles.blocRadioGroup}>
+                <Text style={styles.txtRadio}> Mâle</Text>
+                <Text style={styles.txtRadio}> Femelle</Text>
+              </View>
+              <View style={styles.blocRadioGroup}>
+                <Text style={styles.txtRadio}> Chien</Text>
+                <Text style={styles.txtRadio}> Chat</Text>
+              </View>
             </View>
 
             <View style={styles.bloc}>
-                <View style={[styles.calendar, styles.bg]}> { /*multiple styles */}
-                    <TextInput value='Date de naissance' style={styles.btnCalendar}></TextInput>
-                    <TouchableOpacity activeOpacity={0.5}>
-                        <Image style={{ width : 15, height:15}} source={require('../assets/calendar.svg')}/>
-                    </TouchableOpacity>
-                </View>
+              <View style={[styles.calendar, styles.bg]}>
+                <TextInput value='Date de naissance' style={styles.btnCalendar}></TextInput>
+                <TouchableOpacity activeOpacity={0.5}>
+                  <Image style={{ width: 15, height: 15 }} source={require('../assets/calendar.svg')} />
+                </TouchableOpacity>
+              </View>
 
-                <View style={[styles.calendar, styles.bg]}>
-                    <TextInput value="Date d'adoption" style={styles.btnCalendar}></TextInput>
-                    <TouchableOpacity activeOpacity={0.5}>
-                        <Image style={{ width : 15, height:15}} source={require('../assets/calendar.svg')}/>
-                    </TouchableOpacity>
-                </View>
+              <View style={[styles.calendar, styles.bg]}>
+                <TextInput value="Date d'adoption" style={styles.btnCalendar}></TextInput>
+                <TouchableOpacity activeOpacity={0.5}>
+                  <Image style={{ width: 15, height: 15 }} source={require('../assets/calendar.svg')} />
+                </TouchableOpacity>
+              </View>
             </View>
 
             <View>
-                <Text style={styles.subtitle}>Votre animal est :</Text>
-                <FlatList
-                    horizontal={true}
-                    data={this.poids}
-                    renderItem={({item}) => <View> <Text>{item.gabarit}</Text><Text> {item.tranche}</Text></View>}
-                    keyExtractor={item => item.gabarit}
-                />
+              <Text style={styles.subtitle}>Votre animal est :</Text>
+              <FlatList
+                horizontal={true}
+                data={this.poids}
+                renderItem={({ item }) => <View><Text>{item.gabarit}</Text><Text> {item.tranche}</Text></View>}
+                keyExtractor={item => item.gabarit}
+              />
             </View>
 
-            {/* ici un selecteur pour cocher les vaccins */}
-            <View style={styles.selVaccin}></View>
-
-            <View style={styles.footer}>
-                <TouchableOpacity activeOpacity={0.5} style={[styles.btnFooter, styles.bg]}>
-                    Revenir en arrière
-                </TouchableOpacity>
-                
-                <TouchableOpacity activeOpacity={0.5} style={[styles.btnFooter, styles.bg]}>
-                    Continuer {this.etape}/4
-                </TouchableOpacity>
+            {/* ici un selecteur multiple pour cocher les vaccins */}
+            <View style={styles.selVaccin}>
+              <Text>Vaccins</Text>
             </View>
+          </View>
+        }
+
+        {this.state.etape == 2 &&
+          <View style={styles.etape}>
+            <Text>Est-ce que votre animal a des allergies et/ou intolérances alimentaires?</Text>
+            <Text style={styles.txtRadio}> Oui</Text>
+            <Text style={styles.txtRadio}> Non</Text>
+            <Text>Si oui, à quoi?</Text>
+            <TextInput></TextInput>
+            <Text>Est-ce que votre animal a des problèmes de santé?</Text>
+            <Text style={styles.txtRadio}> Oui</Text>
+            <Text style={styles.txtRadio}> Non</Text>
+            <Text>Si oui, lesquels?</Text>
+            <TextInput></TextInput>
+          </View>
+        }
+
+        {this.state.etape == 3 &&
+          <View style={styles.etape}>
+            <Text>Est-ce que votre animal a des médicaments à consommer?</Text>
+            <Text style={styles.txtRadio}> Oui</Text>
+            <Text style={styles.txtRadio}> Non</Text>
+            <Text>Si oui, lesquels et à quelle fréquence?</Text>
+            <TextInput></TextInput>
+            <Text>Veuillez indiquer la date de la dernière consultation vétérinaire de votre animal.</Text>
+            <TextInput value="Date d'adoption" style={styles.btnCalendar}></TextInput>
+            <TouchableOpacity activeOpacity={0.5}>
+              <Image style={{ width: 15, height: 15 }} source={require('../assets/calendar.svg')} />
+            </TouchableOpacity>
+
+            <Text>Vous pouvez saisir une description générale de votre fidèle compagnon/compagnonne :)</Text>
+            <TextInput multiline={true} numberOfLines={5}></TextInput>
+          </View>
+        }
+
+        {this.state.etape == 4 &&
+          <View style={styles.etape}>
+            <Text>Chargez des photos de votre animal</Text>
+            <TouchableOpacity activeOpacity={0.5}>
+              <Text>Importer depuis la Galerie</Text>
+              <Image style={{ width: 15, height: 15 }} source={require('../assets/download.svg')} />
+            </TouchableOpacity>
+          </View>
+        }
+
+        {this.state.etape == 5 &&
+          <View style={styles.etape}>
+            <Text>Vous avez ajouté votre animal!</Text>
+          </View>
+        }
+
+        <View style={styles.footer}>
+          {this.state.etape == 5 &&
+            <TouchableOpacity activeOpacity={0.5} style={[styles.btnFooter, styles.bg]} onPress={() => this.setState({ etape: 1 })}>
+              <Text>Ajouter un autre animal</Text>
+            </TouchableOpacity>
+          }
+          {this.state.etape > 1 && this.state.etape < 5 &&
+            <TouchableOpacity activeOpacity={0.5} style={[styles.btnFooter, styles.bg]} onPress={() => this.setState({ etape: this.state.etape - 1 })}>
+              <Text>Revenir en arrière</Text>
+            </TouchableOpacity>
+          }
+
+          {this.state.etape < 4 &&
+            <TouchableOpacity activeOpacity={0.5} style={[styles.btnFooter, styles.bg]} onPress={() => this.setState({ etape: this.state.etape + 1 })}>
+              <Text>Continuer {this.state.etape}/4</Text>
+            </TouchableOpacity>
+          }
+
+          {this.state.etape == 4 &&
+            <TouchableOpacity activeOpacity={0.5} style={[styles.btnFooter, styles.bg]} onPress={() => this.setState({ etape: this.state.etape + 1 })}>
+              <Text>Ajouter mon animal {this.state.etape}/4</Text>
+            </TouchableOpacity>
+          }
 
 
+          {this.state.etape == 5 &&
+            <TouchableOpacity activeOpacity={0.5} style={[styles.btnFooter, styles.bg]} >
+              <Text>Accueil</Text>
+            </TouchableOpacity>
+          }
         </View>
-        <View style={styles.etape2}></View>
-        <View style={styles.etape3}></View>
-        <View style={styles.etape4}></View>
-    </SafeAreaView>
+      </SafeAreaView>
     )
   }
 }
@@ -105,21 +175,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
-  etape1 : {
+  etape: {
     alignItems: 'center',
   },
-  etape2: {
-
-  }, 
-  etape3: {
-
-  },
-  etape4: {
-
-  },
   bloc: {
-    alignItems : 'center',
-    marginBottom : 40,
+    alignItems: 'center',
+    marginBottom: 40,
   },
   pageTitle: {
     fontSize: 26,
@@ -128,24 +189,24 @@ const styles = StyleSheet.create({
   btnInput: {
     backgroundColor: '#ddd',
     padding: 8,
-    paddingLeft : 20,
+    paddingLeft: 20,
     width: 250,
     minHeight: 50
   },
   btnCalendar: {
     backgroundColor: '#ddd',
     padding: 8,
-    paddingLeft : 20,
+    paddingLeft: 20,
     width: 240,
     minHeight: 50
   },
   calendar: {
-    flexDirection : 'row',
+    flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 10,
     paddingRight: 8,
     minHeight: 50,
-  }, 
+  },
   bg: {
     backgroundColor: '#ddd',
   },
@@ -157,24 +218,24 @@ const styles = StyleSheet.create({
   },
   blocRadioGroup: {
     flexDirection: 'row',
-    justifyContent : 'space-between',
+    justifyContent: 'space-between',
     marginBottom: 40,
   },
   subtitle: {
     textAlign: 'center',
-    fontWeight : '700',
-    marginBottom : 30,
+    fontWeight: '700',
+    marginBottom: 30,
   },
   selVaccin: {
     marginBottom: 30
   },
   footer: {
-    
+
   },
   btnFooter: {
-    marginBottom : 10,
+    marginBottom: 10,
     padding: 8,
-    paddingLeft : 20,
+    paddingLeft: 20,
     width: 250,
     textAlign: 'center',
   }
