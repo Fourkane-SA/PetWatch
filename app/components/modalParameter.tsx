b9897805c23e8171707af77882a106875f4594f5
Océane Kalambay <oceane.kalambay@etu.univ-lyon1.fr>
2023-01-31 14:36:16 +0100

    inté modal parametres

--- app/components/modalParameter.tsx
+++ app/components/modalParameter.tsx
@@ -1,87 +1,101 @@
-import React, {Component} from 'react';
-import {Modal, StyleSheet, Text, Pressable, View, TouchableOpacity} from 'react-native';
+import React, { Component } from 'react';
+import { Modal, StyleSheet, Text, Pressable, View, TouchableOpacity, Dimensions } from 'react-native';
+
+var width = Dimensions.get('window').width; //full width
+var height = Dimensions.get('window').height; //full height
 
 
 /*TODO Clique sur deconnexion appel fct deconnexion, clique sur modifier profil redirige sur le bon lien selon role utilisateur */
 
 class ModalParameter extends Component {
-  state = {
-    modalVisible: true,
-  };
+    state = {
+        modalVisible: true,
+    };
 
-  render() {
-    const {modalVisible} = this.state;
-    return (
-      <View style={styles.centeredView}>
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            this.setState({modalVisible: !modalVisible});
-          }}>
-          <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-            <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={() => this.setState({modalVisible: !modalVisible})}>
-                <Text style={styles.textStyle}>X</Text>
-              </Pressable>
+    render() {
+        const { modalVisible } = this.state;
+        return (
+            <View style={styles.centeredView}>
+                <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        this.setState({ modalVisible: !modalVisible });
+                    }}>
+                    <View style={styles.modal}>
+                        <View style={styles.modal}>
+                            <Pressable
+                                style={[styles.button, styles.buttonClose]}
+                                onPress={() => this.setState({ modalVisible: !modalVisible })}>
+                                <Text style={styles.textStyle}>X</Text>
+                            </Pressable>
 
-              <TouchableOpacity style={styles.profil}>Modifier mon profil</TouchableOpacity>
-              <TouchableOpacity style={styles.deconnexion}>Déconnexion</TouchableOpacity>
+                            <TouchableOpacity style={styles.profil}>Modifier mon profil</TouchableOpacity>
+                            <TouchableOpacity style={styles.deconnexion}>Déconnexion</TouchableOpacity>
+                        </View>
+                    </View>
+                </Modal>
             </View>
-          </View>
-        </Modal>
-      </View>
-    );
-  }
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: '#F194FF',
-  },
-  buttonClose: {
-    backgroundColor: '#2196F3',
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  profil: {
+    modal: {
+        width: width,
+        minHeight: height,
+        margin: 'auto',
+        padding: 20,
+    },
 
-  },
-  deconnexion: {
-
-  },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+        width: 50,
+        alignSelf: 'flex-end',
+        marginRight: '5%',
+    },
+    buttonClose: {
+        backgroundColor: '#CEEAF0',
+        marginBottom: 30,
+    },
+    textStyle: {
+        color: '#000',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    profil: {
+        minHeight: 50,
+        backgroundColor: '#FFF6E3',
+        borderRadius: 5,
+        marginRight: '5%',
+        color: '#000',
+        fontSize: 16,
+        textAlign: 'center',
+        justifyContent: 'center',
+        fontWeight: 500,
+        marginBottom: 30,
+    },
+    deconnexion: {
+        minHeight: 50,
+        backgroundColor: '#FAD4D4',
+        borderRadius: 5,
+        marginRight: '5%',
+        color: '#000',
+        fontSize: 16,
+        textAlign: 'center',
+        justifyContent: 'center',
+        fontWeight: 500,
+        marginBottom: 30,
+    },
 });
 
 export default ModalParameter;
